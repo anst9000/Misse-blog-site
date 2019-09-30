a747b2c1f1f2e43b47e286e622b336b88a74ba1d
Anders Stromberg <coltlaboy@yahoo.se>
2019-09-30 12:37:59 +0200

    Blog app structure is done

---
+++ src/js/main.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+  //do work
+  console.log('Loaded done')
+
+  function showAllPosts() {
+    // let output = '<h3>Posts</h3>'
+
+    fetch('../res/misse.json')
+      .then((resp) => resp.json()) // Transform the data into json
+      .then((data) => {
+        console.log(data)
+        let output = '<ul>'
+
+        data.forEach((item) => {
+          // console.log(item.date)
+          output += `
+            <li>
+              <img src="img/image_01.jpg" alt="">
+                <div class="content">
+                  <span>${ item.date}</span>
+                  <a href="">${ item.header}</a>
+                  <p>${ item.content}</p>
+                  <p>${ item.image}</p>
+                </div>
+            </li>`
+        })
+        output += '</ul>'
+        document.getElementById('postList').innerHTML = output
+      })
+
+  }
+  showAllPosts()
+})
